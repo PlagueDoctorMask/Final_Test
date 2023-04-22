069498decda98333221639a8cab76171c08e4a88
ZAPC <Dmitri1.Z@yandex.ru>
2023-04-22 17:51:14 +0300

    Got the result

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.Clear();
 Console.WriteLine("Введите массив строк");
-string[] array = Console.ReadLine()!.Split().ToArray();
+string[] array = Console.ReadLine()!.Split(",").ToArray();
 int N = array.Length;
 int count = 0;
 for(int i = 0; i < N; i++)
@@ -8,3 +8,14 @@ for(int i = 0; i < N; i++)
     if(array[i].Length <= 3)
         count++;
 }
+string[] result_array = new string[count];
+int a = 0;
+for(int i = 0; i < N; i++)
+{
+    if(array[i].Length <= 3)
+    {
+        result_array.SetValue(array[i],a);
+        a++;
+    }
+}
+Console.WriteLine($"[{string.Join("," , result_array)}]");
