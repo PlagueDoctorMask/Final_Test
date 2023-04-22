c106ca84184fbd25756f740784a8a62c0fb56356
ZAPC <Dmitri1.Z@yandex.ru>
2023-04-22 15:51:38 +0300

    Got the number of necessary elements

--- Program.cs
+++ Program.cs
@@ -2,4 +2,9 @@
 Console.WriteLine("Введите массив строк");
 string[] array = Console.ReadLine()!.Split().ToArray();
 int N = array.Length;
-
+int count = 0;
+for(int i = 0; i < N; i++)
+{
+    if(array[i].Length <= 3)
+        count++;
+}
